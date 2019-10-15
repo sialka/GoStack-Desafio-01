db273d161b07978ed2ce5f37c7916bca7cf34374
sidnei monteiro <sialkas@gmail.com>
2019-10-15 16:47:36 -0300

    chore: Rota para cadastro de projeto

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const server = express();
 
 server.use(express.json());
 
+const projects = [];
+
+/** Routes */
+
 server.get("/", (req, res) => {
   return res.send("Server Running");
+});
+
+// Criando Projeto
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
 });
 
 server.listen("3333", () => {
